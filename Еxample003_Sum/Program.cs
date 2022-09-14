a0cb06e4f633953ed846d0bc29a83a1c650dffe4
anton krivolapov <akrivola@ya.ru>
2022-09-14 14:38:18 +0300

    commit

--- Еxample003_Sum/Program.cs
+++ Еxample003_Sum/Program.cs
@@ -1,3 +1,4 @@
-﻿int numberA = 3;
-int numberВ = 5;
-Console.WriteLine(numberA + numberВ);
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+int result = numberA + numberB;
+Console.WriteLine(numberA);
